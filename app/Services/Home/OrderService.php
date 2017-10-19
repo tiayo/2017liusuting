e5f19bdde66d640499b39656b0d54094745777d9
中国天妖 <656861622@qq.com>
2017-10-19 11:37:00 +0800

    修改错误

--- app/Services/Home/OrderService.php
+++ app/Services/Home/OrderService.php
@@ -3,9 +3,7 @@
 namespace App\Services\Home;
 
 use App\Repositories\CommodityRepository;
-use App\Repositories\OrderDetailRepository;
 use App\Repositories\OrderRepository;
-use Illuminate\Support\Facades\Auth;
 use Exception;
 
 class OrderService
@@ -18,50 +16,7 @@ class OrderService
         $this->order = $order;
         $this->commodity = $commodity;
     }
-
-    public function add()
-    {
-        throw_if(empty(Auth::user()['address']) || empty(Auth::user()['phone']), Exception::class, '您没有地址和电话！');
-
-        //获取当前用户购物车所有商品
-        $cars = $this->car->get();
-
-        //构造订单
-        $order['user_id'] = Auth::id();
-        $order['name'] = Auth::user()['name'];
-        $order['address'] = Auth::user()['address'];
-        $order['phone'] = Auth::user()['phone'];
-        $order['price'] = $this->car->total_price($cars);
-        $order['type'] = 1;
-        $order['status'] = 1; //测试：默认为已付款
-
-        //创建订单
-        $id = $this->order->create($order)->id;
-
-        //创建订单详情以及减少库存
-        foreach ($cars as $car) {
-            //减少库存操作
-            $this->commodity->decrement($car['num']);
-
-            //订单详情数据
-            $order_detail['order_id'] = $id;
-            $order_detail['user_id'] = $car['user_id'];
-            $order_detail['commodity_id'] = $car['commodity_id'];
-            $order_detail['num'] = $car['num'];
-            $order_detail['price'] = $car['price'];
-            $order_detail['remark'] = $car['remark'];
-            $order_detail['status'] = $car['status'];
-
-            //写入订单详情数据库
-            $this->orderDetail->create($order_detail);
-
-            //删除购物车
-            $this->car->destroy($car['id']);
-        }
-
-        return true;
-    }
-
+    
     /**
      * 获取符合条件的订单
      *
